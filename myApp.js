3f35869f6497aaeb29feb96dcba5d535a690ff58
Ezekiel Gray <ezekiel.gray@zoho.com>
2022-04-03 22:06:41 -0400

    Add to existing  file

--- myApp.js
+++ myApp.js
@@ -12,14 +12,14 @@ app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
 
-    app.get('/json', function(req, res){
+    app.get('/json', function(req, res) {
         var jsonResponse = { "message": "Hello json" };
         
-        if(process.env.MESSAGE_STYLE === 'uppercase'){
-            return jsonResponse.message = jsonResponse.message.toUpperCase();
-        }
-            return res.json(jsonResponse);
-        });
+        if (process.env.VAR_NAME === "allCaps") {
+            response = "Hello World".toUpperCase();
+          } else {
+            response = "Hello World";
+          });
 
 
 
