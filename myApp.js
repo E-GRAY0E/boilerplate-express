9c62fadb3ffa86fb92e8b12da16a3bc5a9919e3e
Ezekiel Gray <ezekiel.gray@zoho.com>
2022-04-03 20:51:14 -0400

    Add to existing file

--- myApp.js
+++ myApp.js
@@ -8,11 +8,12 @@ app.get("/",
     res.sendFile(__dirname + "/views/index.html");
 });
 
+app.use(express.static(__dirname + "/public"));
 app.use("/public", express.static(__dirname + "/public"));
 
 
     app.get('/json', (req, res)=>{
-        if(process.env.MESSAGE_STYLE=='uppercase'){
+        if(process.env.MESSAGE_STYLE === 'uppercase'){
             res.json(
                 { "message": "HELLO JSON"}
             )
