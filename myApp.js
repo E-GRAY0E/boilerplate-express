7b107edad1438888bbd7cc39adb3ba345b549fd1
Ezekiel Gray <ezekiel.gray@zoho.com>
2022-04-03 20:44:39 -0400

    Add to existing file

--- myApp.js
+++ myApp.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var app = express();
+var bGround = require('fcc-express-bground');
+require('dotenv').config()
 
 app.get("/",
     function(req, res) {
@@ -8,14 +10,18 @@ app.get("/",
 
 app.use("/public", express.static(__dirname + "/public"));
 
-if(process.env.MESSAGE_STYLE=='uppercase'){
+
     app.get('/json', (req, res)=>{
-        return res.json({ "message": "HELLO JSON"})
-    });
-} else {
-    app.get('/json', (req, res)=>{
-        return res.json({ "message": "Hello json"})
-    });}
+        if(process.env.MESSAGE_STYLE=='uppercase'){
+            res.json(
+                { "message": "HELLO JSON"}
+            )
+        } else {
+            res.json(
+                { "message": "Hello json"}
+            )
+        }});
+
 
 
 
