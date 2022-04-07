6215cc078f21f4a46ffd47b4dd2817db6147afe5
Ezekiel Gray <ezekiel.gray@zoho.com>
2022-04-06 21:24:12 -0400

    Add to existing file

--- myApp.js
+++ myApp.js
@@ -2,24 +2,24 @@ var express = require('express');
 var app = express();
 var bGround = require('fcc-express-bground');
 require('dotenv').config()
-
+//Serving an HTML file:
 app.get("/",
     function(req, res) {
     res.sendFile(__dirname + "/views/index.html");
 });
-
+//Serving static assets
 app.use(express.static(__dirname + "/public"));
-app.use("/public", express.static(__dirname + "/public"));
+//Serving JSON on a specific route:
+let message = { message: "Hello json"};
+app.get("/json", (req, res) => {
+  if(process.env.MESSAGE_STYLE === 'uppercase') {
+    res.json({"message": "HELLO JSON"})
+  }else{
+    res.json(message)
+  }});
 
 
-    app.get('/json', function(req, res) {
-        var jsonResponse = { "message": "Hello json" };
-        
-        if (process.env.VAR_NAME === "allCaps") {
-            response = "Hello World".toUpperCase();
-          } else {
-            response = "Hello World";
-          });
+
 
 
 
